994d24eac40f53c6c1764703c1949dfe625b2499
Marc Mintel <marc.mintel@sevenval.com>
2018-01-29 10:51:13 +0100

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
+import { rehydrate } from 'fela-dom'
 import { Provider } from 'react-fela'
 import { createRenderer } from 'fela'
 
-const renderer = createRenderer()
 
 exports.replaceRouterComponent = ({ history }) => {
+  const renderer = createRenderer()
+  rehydrate(renderer)
   const ConnectedRouterWrapper = ({ children }) => (
     <Provider renderer={renderer}>
       <Router history={history}>{children}</Router>
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,32 @@
 import React from "react"
 import { renderToString } from "react-dom/server"
+import { Provider } from 'react-fela'
+
 import { createRenderer } from 'fela'
-import { renderToMarkup } from 'fela-dom'
+import { renderToMarkup, renderToSheetList } from 'fela-dom'
+
+const renderer = createRenderer()
 
 exports.replaceRenderer = ({
   bodyComponent,
   replaceBodyHTMLString,
   setHeadComponents,
 }) => {
-  const renderer = createRenderer()
-  const bodyHTML = renderToString(bodyComponent);
-  const styleMarkup = renderToMarkup(renderer)
+  const bodyHTML = renderToString(
+    <Provider renderer={renderer}>
+      {bodyComponent}
+    </Provider>
+  );
+  const sheetList = renderToSheetList(renderer)
+  const elements = sheetList.map(({ type, css, media, support }) =>
+    <style
+      dangerouslySetInnerHTML={{ __html: css }}
+      data-fela-type={type}
+      data-fela-support={support}
+      key={`${type}-${media}`}
+      media={media}
+    />
+  )
   replaceBodyHTMLString(bodyHTML)
-  setHeadComponents([styleMarkup])
+  setHeadComponents(elements)
 }
